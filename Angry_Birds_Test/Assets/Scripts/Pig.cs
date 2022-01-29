36c4e6dd10d9380366764513b388bb558da3ccc3
GD_Zero <95521692+GD-Zero@users.noreply.github.com>
2022-01-29 12:15:57 +0300

    Test done.
    the game works, but with some bugs.

---
+++ Angry_Birds_Test/Assets/Scripts/Pig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Pig : MonoBehaviour
+{
+    [SerializeField] private float stamina = 1f; // выносливость свиньи
+    //public UnityEvent PigAud;
+    private void OnCollisionEnter2D(Collision2D collision) // метод для столкновения
+    {
+        //if (PigAud != null)
+        //{
+        //    PigAud.Invoke();
+        //}
+        if (collision.relativeVelocity.magnitude > stamina) // если сила удара больше, чем выносливость
+        {
+            Destroy(gameObject);// то объект удаляется
+        }
+    }
+}
